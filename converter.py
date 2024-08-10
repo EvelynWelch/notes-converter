cf09593563c28d96bf729cc7f908450eaac65031
Evelyn <evelynwelch.dev@gmail.com>
2024-08-10 00:05:23 -0600

    created utility.py and moved the module unspecific functions there (read and write file, and terminal arguement manager

--- converter.py
+++ converter.py
@@ -6,10 +6,11 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_groq import ChatGroq
 
 import private
+import utilities
 
 # TODO: figure out what exactly i need to tell groq for my use case. right now it is the one from the tutorial
 MESSAGE_CONTENT = "Give me a list of all the proper nouns, and where they are mentioned in the following text: "
-# This is what it responded with, the numbers in the parentheses are what scentences that proper noun is mentioned.
+# This is what it responded with, the numbers in the parentheses are what scentences(?) that proper noun is mentioned.
 
 # Here is the list of proper nouns with their corresponding mentions in the text:
 # 
@@ -24,41 +25,6 @@ MESSAGE_CONTENT = "Give me a list of all the proper nouns, and where they are me
 
 # this is close to what i want, though it to be able to also know what chunks of text are talking about the proper noun without them being mentioned.
 
-def read_file_as_string(file_path):
-    '''function to read a file as a string'''
-    try:
-        with open(file_path, 'r') as file:
-            file_contents = file.read()
-        return file_contents
-    except FileNotFoundError:
-        return "Error: The file was not found."
-    except IOError:
-        return "Error: An IOError occurred while reading the file."
-    
-def write_data_to_file(file_path, data):
-    '''funtion to write data to a file'''
-    try:
-        with open(file_path, 'w') as file:
-            file.write(data)
-        print(f"Data successfully written to {file_path}")
-    except IOError:
-        print("Error: An IOError occurred while writing to the file.")
-
-def get_terminal_args():
-    '''handles setting up argparse, and returns the results'''
-    arg_parser = argparse.ArgumentParser(description="A script to send a note file to an groq for processing.")
-    arg_parser.add_argument("file_path", help="the note file path")
-    arg_parser.add_argument("--output_file_path", help="the file you wish to output the response to")
-    args = arg_parser.parse_args()
-    return args
-
-def output_handler(args, parsed_result):
-    '''prints output unless an output_file_path is provided'''   
-    if args.output_file_path:
-        write_data_to_file(args.output_file_path, parsed_result)
-        print(f"results written to {args.output_file_path}")
-    else:
-        print(parsed_result)
 
 def invoke_groq_request(model: ChatGroq, system_message: SystemMessage, human_message: HumanMessage):
     result = model.invoke([system_message, human_message])
@@ -70,9 +36,9 @@ if __name__ == "__main__":
     # choose what model to use
     model = ChatGroq(model="llama3-8b-8192")
     # get passed args
-    args = get_terminal_args()
+    args = utilities.get_terminal_args()
 
-    note = read_file_as_string(args.file_path)
+    note = utilities.read_file_as_string(args.file_path)
 
     result = invoke_groq_request(
         model=model, 
@@ -83,4 +49,4 @@ if __name__ == "__main__":
     parser = StrOutputParser()
     parsed_result = parser.invoke(result)
  
-    output_handler(args, parsed_result)
+    utilities.output_handler(args, parsed_result)
